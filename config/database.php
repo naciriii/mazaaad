be390da6a2afdcc5dccf43e95a57fe2718a3ec53
naciriii <nnacir1938@hotmail.com>
2018-04-13 00:01:31 +0200

    build status + init test

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'wzqlaim6uog1zrgn'),
+            'username' => env('DB_USERNAME', 'yygdmhim8npenu4b'),
+            'password' => env('DB_PASSWORD', 'ffk3twwdtc3a6vtp'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+<h3>PFE Mazaad Build Status :</h3> [![Build Status](https://travis-ci.org/naciriii/mazaaad.svg?branch=master)](https://travis-ci.org/naciriii/mazaaad)
+<br>
 <p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
 
 <p align="center">
